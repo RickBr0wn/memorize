81cb3f32d740bc931a282fe26f617c3b363e700a
Rick Brown <ricky.brown.00@gmail.com>
2021-02-17 11:36:52 +0000

    adds basic game with grid

---
+++ Memorize/Extensions/Array+Extension.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension Array where Element: Identifiable {
+  func firstIndex(matching element: Element) -> Int? {
+    for index in 0..<self.count {
+      if self[index].id == element.id {
+        return index
+      }
+    }
+    return nil
+  }
+}
+
+extension Array {
+  var only: Element? {
+    count == 1 ? first : nil
+  }
+}
--- Memorize/Views/EmojiMemoryGameView.swift
+++ Memorize/Views/EmojiMemoryGameView.swift
@@ -11,12 +11,11 @@ struct EmojiMemoryGameView: View {
   @ObservedObject var viewModel: EmojiMemoryGame
   
   var body: some View {
-    HStack {
-      ForEach(viewModel.cards) { card in
+    Grid(viewModel.cards) { card in
         CardView(card: card)
-          .onTapGesture(perform: { viewModel.choose(card: card) })
+          .onTapGesture { viewModel.choose(card: card) }
           .aspectRatio(2/3, contentMode: .fit)
-      }
+          .padding(5)
     }
     .padding()
     .foregroundColor(Color.orange)
@@ -24,25 +23,41 @@ struct EmojiMemoryGameView: View {
   }
 }
 
-enum General: CGFloat {
-  case cornerRadius = 10.0
-}
-
 struct CardView: View {
   var card: MemoryGame<String>.Card
   
   var body: some View {
     GeometryReader { geometry in
-      ZStack {
-        if card.isFaceUp {
-          RoundedRectangle(cornerRadius: General.cornerRadius.rawValue).fill(Color.white)
-          RoundedRectangle(cornerRadius: General.cornerRadius.rawValue).stroke(lineWidth: 3.0)
-          Text(card.content)
-        } else {
-          RoundedRectangle(cornerRadius: General.cornerRadius.rawValue).fill()
+      self.body(for: geometry.size)
+    }
+  }
+  
+  func body(for size: CGSize) -> some View {
+    ZStack {
+      if card.isFaceUp {
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .fill(Color.white)
+        
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .stroke(lineWidth: edgeLineWidth)
+        
+        Text(card.content)
+      } else {
+        if !card.isMatched {
+          RoundedRectangle(cornerRadius: cornerRadius)
+            .fill()
         }
       }
     }
+    .font(Font.system(size: fontSize(for: size)))
+  }
+  
+  // MARK: Layout Constants
+  let cornerRadius: CGFloat = 10.0
+  let edgeLineWidth: CGFloat = 3.0
+  
+  func fontSize(for size: CGSize) -> CGFloat {
+    min(size.width, size.height) * 0.8
   }
 }
 
